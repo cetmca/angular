3d953f80637324543aa2f828b0f078a9d90142cc
Anilal Kesavan Gopinathan <agopinathan@tccrocks.com>
2019-03-08 15:29:43 -0500

    Angular code

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'My First Angular App -----';
+  myVar = ' variable';
+}
+
+export class App2Component {
+  title2 = 'App2Component  --  -----';
+  myVar2 = 'App2Component  -- var 2';
+}
---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent,App2Component } from './app.component';
+
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewCmpComponent
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
